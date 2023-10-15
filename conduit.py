430e4f91db2c00b73364db64fc50788c0eb6538a
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-10-15 11:08:25 +1100

    feat: conduit, custom "placeholder" property

--- conduit.py
+++ conduit.py
@@ -2,7 +2,7 @@ import bpy
 
 bl_info = {
     "name": "Conduit",
-    "blender": (2, 80, 0),
+    "blender": (3, 6, 0),
     "category": "Import-Export"
 }
 
@@ -11,7 +11,7 @@ def xyz(value):
     return (value.x, value.y, value.z)
 
 
-class ExportScene(bpy.types.Operator):
+class ExportOperator(bpy.types.Operator):
     bl_idname = "conduit.export"
     bl_label = "Export scene.gltf"
     bl_options = {'REGISTER'}
@@ -35,12 +35,14 @@ class ExportScene(bpy.types.Operator):
             export_cameras=True,
             # Lights.
             export_lights=True,
+            # Custom Properties.
+            export_extras=True,
         )
 
         return {'FINISHED'}
 
 
-class Panel(bpy.types.Panel):
+class ExportPanel(bpy.types.Panel):
     bl_idname = "CONDUIT_PT_panel"
     bl_label = "Conduit"
     bl_space_type = "VIEW_3D"
@@ -48,20 +50,61 @@ class Panel(bpy.types.Panel):
     bl_category = "Conduit"
 
     def draw(self, context):
-        self.layout.operator(ExportScene.bl_idname, text=ExportScene.bl_label)
+        self.layout.operator(ExportOperator.bl_idname, text=ExportOperator.bl_label)
+
+
+class ObjectPropertiesPanel(bpy.types.Panel):
+    bl_idname = "CONDUIT_PT_object_panel"
+    bl_label = "Conduit"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+
+    def draw(self, context):
+        self.layout.prop(
+            context.active_object,
+            "conduit_placeholder",
+            text="Placeholder"
+        )
 
 
 def menu_func_export(self, context):
-    self.layout.operator(ExportScene.bl_idname, text=ExportScene.bl_label)
+    self.layout.operator(ExportOperator.bl_idname, text=ExportOperator.bl_label)
 
+
+#  @bpy.app.handlers.persistent
+#  def obj_init(scene):
+#      if hasattr(bpy.context, "object") and bpy.context.object.placeholder is not None:
+#          print("obj:init", scene)
+#          bpy.context.object.placeholder = None
+
+
+# https://b3d.interplanety.org/en/creating-panels-for-placing-blender-add-ons-user-interface-ui/
+# https://s-nako.work/2020/12/how-to-add-properties-in-your-blender-addon-ui/
+# https://s-nako.work/2020/12/how-to-display-custom-property-on-properties-editor-in-blender-addon/
 
 def register():
-    print("register")
-    bpy.utils.register_class(ExportScene)
-    bpy.utils.register_class(Panel)
+    print("mount")
+    bpy.utils.register_class(ExportOperator)
+    bpy.utils.register_class(ExportPanel)
+
+    bpy.types.Object.conduit_placeholder = bpy.props.PointerProperty(
+        name="ConduitPlaceholder", type=bpy.types.Object)
+    #  bpy.app.handlers.depsgraph_update_post.append(obj_init)
+
+    bpy.utils.register_class(ObjectPropertiesPanel)
+
+    print("mount:done")
 
 
 def unregister():
-    print("unregister")
-    bpy.utils.unregister_class(ExportScene)
-    bpy.utils.unregister_class(Panel)
+    print("unmount")
+    bpy.utils.unregister_class(ExportOperator)
+    bpy.utils.unregister_class(ExportPanel)
+
+    del bpy.types.Object.conduit_placeholder
+    #  bpy.app.handlers.depsgraph_update_post.remove(obj_init)
+
+    bpy.utils.unregister_class(ObjectPropertiesPanel)
+
+    print("unmount:done")
--- index.ts
+++ index.ts
@@ -1,19 +1,119 @@
 import * as THREE from "three";
+import * as CANNON from "cannon-es";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
 import { OrbitControls } from "three/addons/controls/OrbitControls";
+
 import sceneGltf from "./scene.gltf?url";
 
-{
-  // const fov = 45;
-  // const aspect = window.innerWidth / window.innerHeight;
-  // const near = 0.01;
-  // const far = 10;
-  // const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-}
+const canvas = document.querySelector("#app")!;
 
+let camera = new THREE.PerspectiveCamera();
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("grey");
 
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+world.broadphase = new CANNON.SAPBroadphase(world);
+world.defaultContactMaterial.friction = 0;
+
+const groundMaterial = new CANNON.Material("ground");
+// groundMaterial.friction = 0.25
+// groundMaterial.restitution = 0.25
+
+const getVec3Tuple = (
+  vec3: Record<"x" | "y" | "z", number>,
+): [x: number, y: number, z: number] => {
+  const { x, y, z } = vec3;
+  return [x, y, z];
+};
+
+const actors: {
+  mesh: THREE.Mesh;
+  body: CANNON.Body;
+}[] = [];
+
+{
+  const url = sceneGltf;
+  const gltfLoader = new GLTFLoader();
+  gltfLoader.load(url, (gltf) => {
+    console.log(gltf);
+    camera = gltf.cameras[0];
+    const controls = new OrbitControls(camera, canvas);
+    controls.update();
+    scene.add(gltf.scene);
+    gltf.scene.children.forEach((child) => {
+      if (child.type === "Mesh") {
+        const mesh = child as THREE.Mesh;
+
+        if (!mesh.geometry.boundingBox) {
+          return;
+        }
+
+        mesh.receiveShadow = true;
+
+        const mass = child.name === "Plane" ? 0 : 1;
+
+        const body = new CANNON.Body({ mass, material: groundMaterial });
+        body.position.copy(mesh.position as unknown as CANNON.Vec3);
+        const shape = new CANNON.Box(
+          new CANNON.Vec3(
+            ...getVec3Tuple(
+              mesh.geometry.boundingBox.max.divide(new THREE.Vector3(2, 1, 2)),
+            ),
+          ),
+        );
+        body.addShape(shape);
+        world.addBody(body);
+
+        actors.push({ mesh, body });
+      }
+    });
+
+    console.log(actors);
+  });
+}
+
+// const groundWheelContactMaterial = new CANNON.ContactMaterial(
+//   groundMaterial,
+//   wheelMaterial,
+//   {
+//     friction: 0.3,
+//     restitution: 0,
+//     contactEquationStiffness: 1000,
+//   },
+// );
+
+// world.addContactMaterial(groundWheelContactMaterial);
+
+// const wheelMaterial = new CANNON.Material('wheelMaterial')
+// wheelMaterial.friction = 0.25
+// wheelMaterial.restitution = 0.25
+
+const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animation);
+
+function animation(_dt: number) {
+  world.fixedStep();
+
+  actors.forEach(({ mesh, body }) => {
+    mesh.position.copy(body.position as unknown as THREE.Vector3);
+    mesh.quaternion.copy(body.quaternion as unknown as THREE.Quaternion);
+  });
+
+  renderer.render(scene, camera);
+}
+
+// camera
+// {
+//   const fov = 45;
+//   const aspect = window.innerWidth / window.innerHeight;
+//   const near = 0.01;
+//   const far = 10;
+//   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+// }
+
+// skylight
 // {
 //   const skyColor = 0xb1e1ff; // light blue
 //   const groundColor = 0xb97a20; // brownish orange
@@ -21,26 +121,3 @@ scene.background = new THREE.Color("grey");
 //   const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
 //   scene.add(light);
 // }
-
-const canvas = document.querySelector("#app")!;
-
-let camera = new THREE.PerspectiveCamera();
-
-const url = sceneGltf;
-const gltfLoader = new GLTFLoader();
-gltfLoader.load(url, (gltf) => {
-  console.log(gltf);
-  const root = gltf.scene;
-  camera = gltf.cameras[0];
-  const controls = new OrbitControls(camera, canvas);
-  controls.update();
-  scene.add(root);
-});
-
-const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animation);
-
-function animation(_dt: number) {
-  renderer.render(scene, camera);
-}
