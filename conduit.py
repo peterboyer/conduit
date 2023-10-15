60d21c228ef2d5205cccfe6d6a57282945b49cde
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-10-15 15:14:35 +1100

    chore: retab py

--- conduit.py
+++ conduit.py
@@ -1,110 +1,203 @@
 import bpy
 
 bl_info = {
-    "name": "Conduit",
-    "blender": (3, 6, 0),
-    "category": "Import-Export"
+	"name": "Conduit",
+	"blender": (3, 6, 0),
+	"category": "Import-Export"
 }
 
 
-def xyz(value):
-    return (value.x, value.y, value.z)
-
-
 class ExportOperator(bpy.types.Operator):
-    bl_idname = "conduit.export"
-    bl_label = "Export scene.gltf"
-    bl_options = {'REGISTER'}
+	bl_idname = "conduit.export"
+	bl_label = "Export scene.gltf"
+	bl_options = {'REGISTER'}
 
-    def execute(self, context):
-        if not bpy.data.is_saved:
-            self.report({'WARNING'}, "Scene must be saved first.")
-            return {'CANCELLED'}
+	def execute(self, ctx):
+		if not bpy.data.is_saved:
+			self.report({'WARNING'}, "Scene must be saved first.")
+			return {'CANCELLED'}
 
-        filepath = bpy.path.abspath("//") + "scene.gltf"
+		# (object, conduit_placeholder,)[]
+		restore_placeholders = []
 
-        # https://docs.blender.org/api/current/bpy.ops.export_scene.html
-        bpy.ops.export_scene.gltf(
-            filepath=filepath,
-            check_existing=False,
-            # Unitless, works best for Three.
-            export_import_convert_lighting_mode="COMPAT",
-            # Single file.
-            export_format="GLTF_EMBEDDED",
-            # Cameras.
-            export_cameras=True,
-            # Lights.
-            export_lights=True,
-            # Custom Properties.
-            export_extras=True,
-        )
+		for object in bpy.data.objects:
+			value = object.conduit_placeholder
+			if value is not None:
+				object.instance_type = 'NONE'
+				object.instance_collection = None
+				restore_placeholders.append({object, value})
 
-        return {'FINISHED'}
+		filepath = bpy.path.abspath("//") + "scene.gltf"
+
+		# https://docs.blender.org/api/current/bpy.ops.export_scene.html
+		bpy.ops.export_scene.gltf(
+			filepath=filepath,
+			check_existing=False,
+			# Unitless, works best for Three.
+			export_import_convert_lighting_mode="COMPAT",
+			# Single file.
+			export_format="GLTF_EMBEDDED",
+			# Cameras.
+			export_cameras=True,
+			# Lights.
+			export_lights=True,
+			# Custom Properties.
+			export_extras=True,
+		)
+
+		for restore_placeholder in restore_placeholders:
+			object, value = restore_placeholder
+			object.instance_type = 'COLLECTION'
+			object.instance_collection = value
+
+		return {'FINISHED'}
 
 
 class ExportPanel(bpy.types.Panel):
-    bl_idname = "CONDUIT_PT_panel"
-    bl_label = "Conduit"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-    bl_category = "Conduit"
+	bl_idname = "CONDUIT_PT_panel"
+	bl_label = "Conduit"
+	bl_space_type = "VIEW_3D"
+	bl_region_type = "UI"
+	bl_category = "Conduit"
 
-    def draw(self, context):
-        self.layout.operator(ExportOperator.bl_idname, text=ExportOperator.bl_label)
+	def draw(self, ctx):
+		self.layout.operator(
+			ExportOperator.bl_idname,
+			text=ExportOperator.bl_label,
+		)
 
 
 class ObjectPropertiesPanel(bpy.types.Panel):
-    bl_idname = "CONDUIT_PT_object_panel"
-    bl_label = "Conduit"
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
-    bl_context = "object"
+	bl_idname = "CONDUIT_PT_object_panel"
+	bl_label = "Conduit"
+	bl_space_type = "PROPERTIES"
+	bl_region_type = "WINDOW"
+	bl_context = "object"
 
-    def draw(self, context):
-        self.layout.prop(
-            context.active_object,
-            "conduit_placeholder",
-            text="Placeholder"
-        )
+	@classmethod
+	def poll(cls, ctx):
+		return (ctx.object.type in {'EMPTY'})
+
+	def draw(self, ctx):
+		self.layout.prop(
+			ctx.object,
+			"conduit_placeholder",
+			text="Placeholder",
+		)
 
 
-def menu_func_export(self, context):
-    self.layout.operator(ExportOperator.bl_idname, text=ExportOperator.bl_label)
+def on_placeholder_update(self, ctx):
+	value = self.conduit_placeholder
+	if value is None:
+		self.instance_type = 'NONE'
+		self.instance_collection = None
+	else:
+		self.instance_type = 'COLLECTION'
+		self.instance_collection = value
 
 
-#  @bpy.app.handlers.persistent
-#  def obj_init(scene):
-#      if hasattr(bpy.context, "object") and bpy.context.object.placeholder is not None:
-#          print("obj:init", scene)
-#          bpy.context.object.placeholder = None
+class SceneActorItemProperty(bpy.types.PropertyGroup):
+	pass
+
+
+class SceneActorAddOperator(bpy.types.Operator):
+	bl_idname = "conduit.scene_actors_add"
+	bl_label = "Add a new actor type"
+
+	def execute(self, ctx):
+		ctx.scene.conduit_actors.add()
+		return {'FINISHED'}
+
+
+class SceneActorRemoveOperator(bpy.types.Operator):
+	bl_idname = "conduit.scene_actors_remove"
+	bl_label = "Remove an existing actor type"
+
+	def execute(self, ctx):
+		ctx.scene.conduit_actors.remove(ctx.scene.conduit_actors_active_index)
+		return {'FINISHED'}
+
+
+class ScenePropertiesPanel(bpy.types.Panel):
+	bl_idname = "CONDUIT_PT_scene_panel"
+	bl_label = "Conduit"
+	bl_space_type = "PROPERTIES"
+	bl_region_type = "WINDOW"
+	bl_context = "scene"
+
+	def draw(self, ctx):
+		row = self.layout.row()
+
+		template_list_options = {
+			"list_type": "UI_UL_list",
+			"list_id": "conduit_actors",
+			"data": (ctx.scene, "conduit_actors"),
+			"data_active_index": (ctx.scene, "conduit_actors_active_index"),
+		}
+
+		col = row.column()
+		col.template_list(
+			template_list_options["list_type"],
+			template_list_options["list_id"],
+			template_list_options["data"][0],
+			template_list_options["data"][1],
+			template_list_options["data_active_index"][0],
+			template_list_options["data_active_index"][1],
+			rows=4
+		)
+
+		col = row.column(align=True)
+		col.operator(SceneActorAddOperator.bl_idname, icon='ADD', text="")
+		col.operator(SceneActorRemoveOperator.bl_idname, icon='REMOVE', text="")
 
 
 # https://b3d.interplanety.org/en/creating-panels-for-placing-blender-add-ons-user-interface-ui/
 # https://s-nako.work/2020/12/how-to-add-properties-in-your-blender-addon-ui/
 # https://s-nako.work/2020/12/how-to-display-custom-property-on-properties-editor-in-blender-addon/
+# https://en.wikibooks.org/wiki/Blender_3D%3A_Noob_to_Pro/Advanced_Tutorials/Python_Scripting/Addon_Custom_Property
 
 def register():
-    print("mount")
-    bpy.utils.register_class(ExportOperator)
-    bpy.utils.register_class(ExportPanel)
+	# types
+	bpy.utils.register_class(SceneActorItemProperty)
 
-    bpy.types.Object.conduit_placeholder = bpy.props.PointerProperty(
-        name="ConduitPlaceholder", type=bpy.types.Object)
-    #  bpy.app.handlers.depsgraph_update_post.append(obj_init)
+	# props
+	bpy.types.Scene.conduit_actors = \
+		bpy.props.CollectionProperty(
+			type=SceneActorItemProperty,
+		)
+	bpy.types.Scene.conduit_actors_active_index = \
+		bpy.props.IntProperty(
+			default=0
+		)
+	bpy.types.Object.conduit_placeholder = \
+		bpy.props.PointerProperty(
+			type=bpy.types.Collection,
+			update=on_placeholder_update,
+		)
 
-    bpy.utils.register_class(ObjectPropertiesPanel)
+	# actions
+	bpy.utils.register_class(ExportOperator)
+	bpy.utils.register_class(SceneActorAddOperator)
+	bpy.utils.register_class(SceneActorRemoveOperator)
 
-    print("mount:done")
+	# panels
+	bpy.utils.register_class(ScenePropertiesPanel)
+	bpy.utils.register_class(ObjectPropertiesPanel)
+	bpy.utils.register_class(ExportPanel)
 
 
 def unregister():
-    print("unmount")
-    bpy.utils.unregister_class(ExportOperator)
-    bpy.utils.unregister_class(ExportPanel)
+	# panels
+	bpy.utils.unregister_class(ScenePropertiesPanel)
+	bpy.utils.unregister_class(ObjectPropertiesPanel)
+	bpy.utils.unregister_class(ExportPanel)
 
-    del bpy.types.Object.conduit_placeholder
-    #  bpy.app.handlers.depsgraph_update_post.remove(obj_init)
+	# actions
+	bpy.utils.unregister_class(ExportOperator)
 
-    bpy.utils.unregister_class(ObjectPropertiesPanel)
+	# props
+	del bpy.types.Scene.conduit_actors
+	del bpy.types.Object.conduit_placeholder
 
-    print("unmount:done")
+	# types
+	bpy.utils.unregister_class(SceneActorItemProperty)
